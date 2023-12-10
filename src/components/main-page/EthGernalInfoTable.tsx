99b40d6ef25591607bce3897716cd1d24d22c5fe
saket verma <saketverma831@gmail.com>
2023-12-10 20:55:33 +0530

    Updated packages

---
+++ src/components/main-page/EthGernalInfoTable.tsx
@@ -0,0 +1,22 @@
+export default function EthGernalInfoTable() {
+  return (
+    <>
+      <div className="grid bg-card m-4 p-4 rounded-xl md:grid-cols-3 border  shadow-[rgba(0,_0,_0,_0.4)_0px_0px_40px] shadow-sdo">
+        <div className="py-2 md:mr-4  border-b md:border-b-0 ">
+          <div className="border-b-2 pb-4 border-accent">lolz</div>
+
+          <div className="py-4 ">lolz</div>
+        </div>
+        <div className="py-2 md:px-4 md:border-x-2 ">
+          <div className="border-b pb-4 ">lolz</div>
+
+          <div className="py-4 ">lolz</div>
+        </div>
+
+        <div className="grid place-items-center border-t md:border-t-0 pt-4 ">
+          graph
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/main-page/index.ts
@@ -0,0 +1,1 @@
+export {default as EthGernalInfoTable } from "./EthGernalInfoTable" 
