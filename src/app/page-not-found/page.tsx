0a0f1243249767ed8ab0f661760c0d4f55117e1d
saket verma <saketverma831@gmail.com>
2023-12-10 20:54:06 +0530

    ADDED: default 404 page

---
+++ src/app/page-not-found/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <div className="flex h-full justify-center items-center">
+        <h1 className="text-xl mt-[20vh] font-bold">Page Not Found</h1>
+      </div>
+    </>
+  );
+}
