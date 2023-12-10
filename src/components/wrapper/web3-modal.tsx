0523b7b1185766f4aa37a00a4b08a052fe6bed7e
saket verma <saketverma831@gmail.com>
2023-12-10 20:52:01 +0530

    ADDED: wrapers for react wuery and web3modal

---
+++ src/components/wrapper/web3-modal.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+
+import { WagmiConfig } from "wagmi";
+import { arbitrum, mainnet } from "viem/chains";
+import { ReactNode } from "react";
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = "197f181bd5067cf9349f9f1cc052cf60";
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, arbitrum];
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains });
+
+export function Web3Modal({ children }: { children: ReactNode }) {
+  return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>;
+}
